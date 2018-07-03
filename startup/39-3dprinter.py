92adef52f3382743f872ba74da1d2e320981dd60
Julien Lhermitte <lhermitte@bnl.gov>
2018-07-03 09:31:01 -0400

    Added lab profile

---
+++ startup/39-3dprinter.py
@@ -0,0 +1,19 @@
+from ophyd import EpicsMotor
+
+class Printer3D(Device):
+    Z = Cpt(EpicsMotor,"Mtr")
+    X_platform = Cpt(EpicsMotor,"-ax2Mtr")
+    Y = Cpt(EpicsMotor,"-ax3Mtr")
+    X_printhead1 = Cpt(EpicsMotor,"-ax4Mtr")
+    B = Cpt(EpicsMotor,"-ax5Mtr")
+
+printer3d = Printer3D("alexHost:smaract2", name="printer3d")
+
+class StandardProsilicaWithTIFFtmp(StandardProsilica):
+    tiff = Cpt(TIFFPluginWithFileStore,
+               suffix='TIFF1:',
+               write_path_template='/tmp/%Y/%m/%d/',
+               root='/tmp',
+               reg=db.reg)
+xray_eye_printer = StandardProsilicaWithTIFFtmp("XF:MOBILE-BI{Mir:1}",
+                         name="xray_eye_printer")
